91cb2393305f6540c2f530849c1dc7c31e3cd712
kushangpatel89 <kushangpatel89@gmail.com>
2026-08-27 20:19:43 +0530

    Add files via upload

---
+++ VariableNamingRules.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main(){
+//Variable Naming Rules
+  //1. variable must start from alphabet or _
+    //valid variables
+    int x1 = 5;
+    int x1_2;
+    int _ka236;
+    //invalid variavle examples
+    int 1x ;
+    int 1-c;
+    int a.b;
+    char
+
+//2. special charactors are not allowed in variable name
+ //example !@ # % ^ & * ( ) { } [ ] < > , . ? / ' ; : etc are not allowed
+   int c%;
+   int ^op;
+   int ly#;
+
+//3. Blanks/space not allowed in variable name
+    int kushang patel;
+    int x y;
+    int anky monster;
+
+//4. keywords in its base form are not allowed
+//exp. for , if, else, while , etc not allowerd
+// we can use it as For ELse , iF, While are allowed
+   //invalid
+    int for;
+    int while;
+    int float;
+   //valid
+    int For;
+    int FloaT;
+    int WhiLe;
+
+}
---
+++ variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x; //intialization of variable
+    x=8;
+    cout<<x+12<<endl;
+    int y = 2; //initialization and declaration of variable
+    y = y + x;
+    cout<<y<<endl;
+    int a = 13 ,b = 9;
+    cout<<a<<","<<b<<endl;
+}
