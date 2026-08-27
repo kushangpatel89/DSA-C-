4fa7bc33113036b120c23873bc1abd306ddc072a
kushangpatel89 <kushangpatel89@gmail.com>
2026-08-27 20:48:01 +0530

    Add files via upload

---
+++ Loops/For Loop/1Contents.cpp
@@ -0,0 +1,8 @@
+// loop is a control flow statement 
+//that allows code to be executed repeatedly 
+//based on a given boolean condition.
+// The code inside the loop will continue to execute as long as 
+//the condition evaluates to true. 
+//There are several types of loops in C++, 
+//including for loops, while loops, and do-while loops.
+
